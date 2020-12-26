44c0a57851adc5d02a5c1d33b00a1c6d5f7accac
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-12-26 15:48:44 +0300

    enable async + scheduling

--- tika-server/src/main/java/com/github/zhurlik/tika/DocSearchApplication.java
+++ tika-server/src/main/java/com/github/zhurlik/tika/DocSearchApplication.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author zhurlik@gmail.com
  */
 @SpringBootApplication
 @Slf4j
+@EnableScheduling
+@EnableAsync
 public class DocSearchApplication implements CommandLineRunner {
     public static void main(final String[] args) {
         SpringApplication.run(DocSearchApplication.class, args);
