b01ba16f8563689f9ca85d5cd3bbcce0b295343c
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-12-26 16:05:45 +0300

    upd Tika config

--- tika-server/src/main/java/com/github/zhurlik/tika/config/TikaConfig.java
+++ tika-server/src/main/java/com/github/zhurlik/tika/config/TikaConfig.java
@@ -1,12 +1,19 @@
 package com.github.zhurlik.tika.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tika.Tika;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * A set of the beans related with Tika and required for scanning process.
@@ -14,6 +21,7 @@ import java.util.List;
  * @author zhurlik@gmail.com
  */
 @Configuration
+@Slf4j
 public class TikaConfig {
 
     @Bean
@@ -25,6 +33,24 @@ public class TikaConfig {
     @ConfigurationProperties(prefix = "scanner")
     public ScannerProperties scannerProperties() {
         return new ScannerProperties();
+    }
+
+    @Bean
+    public List<Path> dirs(final ScannerProperties scannerProperties, final ResourceLoader resourceLoader) {
+        return scannerProperties.getResources().stream()
+                .map(resourceLoader::getResource)
+                .map(resource -> {
+                    try {
+                        return resource.getFile();
+                    } catch (IOException e) {
+                        log.warn("A problem with resource:", e);
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .filter(File::isDirectory)
+                .map(File::toPath)
+                .collect(Collectors.toList());
     }
 
     /**
