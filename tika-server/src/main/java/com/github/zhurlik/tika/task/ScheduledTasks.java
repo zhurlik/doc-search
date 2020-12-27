d00a277c575899584bfaabd235eb725b70626d30
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-12-27 21:34:00 +0300

    ping elasticsearch

--- tika-server/src/main/java/com/github/zhurlik/tika/task/ScheduledTasks.java
+++ tika-server/src/main/java/com/github/zhurlik/tika/task/ScheduledTasks.java
@@ -3,9 +3,13 @@ package com.github.zhurlik.tika.task;
 import com.github.zhurlik.tika.event.ScannerEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author zhurlik@gmail.com
@@ -16,10 +20,21 @@ import org.springframework.stereotype.Component;
 public class ScheduledTasks {
 
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final RestHighLevelClient client;
 
-    @Scheduled(initialDelay = 5000, fixedRate = 60000)
+    @Scheduled(initialDelay = 10000, fixedRate = 60000)
     public void rescan() {
         log.info(">> Rescanning...");
         applicationEventPublisher.publishEvent(new ScannerEvent(ScannerEvent.ACTIONS.START));
+    }
+
+    @Scheduled(initialDelay = 5000, fixedRate = 30000)
+    public void pingElasticSearch() {
+        try {
+            final boolean available = client.ping(RequestOptions.DEFAULT);
+            log.info(">> Ping ElasticSearch server: {}", available);
+        } catch (IOException e) {
+            log.warn("A problem with ElasticSearch:", e);
+        }
     }
 }
--- tika-server/src/test/java/com/github/zhurlik/tika/task/ScheduledTasksTest.java
+++ tika-server/src/test/java/com/github/zhurlik/tika/task/ScheduledTasksTest.java
@@ -2,6 +2,7 @@ package com.github.zhurlik.tika.task;
 
 
 import com.github.zhurlik.tika.event.ScannerEvent;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.verify;
 
@@ -24,9 +26,17 @@ class ScheduledTasksTest {
     @Mock
     private ApplicationEventPublisher applicationEventPublisher;
 
+    @Mock
+    private RestHighLevelClient client;
+
     @Test
     void testMain() {
         scheduledTasks.rescan();
         verify(applicationEventPublisher).publishEvent(isA(ScannerEvent.class));
+    }
+
+    @Test
+    void testPing() {
+        assertThrows(NullPointerException.class, () -> scheduledTasks.pingElasticSearch());
     }
 }
