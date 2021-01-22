8c3b7491073c411e7deb3c5dbb46dd9df62e1b8f
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2021-01-22 22:21:08 +0300

    go through files using Flux

--- tika-server/src/main/java/com/github/zhurlik/tika/listener/ScannerListener.java
+++ tika-server/src/main/java/com/github/zhurlik/tika/listener/ScannerListener.java
@@ -1,6 +1,5 @@
 package com.github.zhurlik.tika.listener;
 
-import com.github.zhurlik.tika.config.ScannerProperties;
 import com.github.zhurlik.tika.event.ElasticSearchDocumentEvent;
 import com.github.zhurlik.tika.event.FileEvent;
 import com.github.zhurlik.tika.event.ScannerEvent;
@@ -9,16 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.List;
 
@@ -30,10 +27,6 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 public class ScannerListener {
-
-    private final ScannerProperties scannerProperties;
-    private final ResourceLoader resourceLoader;
-
     private final List<Path> dirs;
     private final ApplicationEventPublisher applicationEventPublisher;
 
@@ -64,16 +57,11 @@ public class ScannerListener {
     private void doScan(final Path dir) {
         log.info(" Scan dir: {}", dir.toString());
         try {
-            Files.walkFileTree(dir, new SimpleFileVisitor<>() {
-                @Override
-                public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
-                    if (file.toFile().isFile() && Files.size(file) > 0) {
-                        // TODO: file attributes
-                        applicationEventPublisher.publishEvent(new FileEvent(file));
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
+            Flux.fromStream(Files.walk(dir, FileVisitOption.FOLLOW_LINKS))
+                    // only files with a content
+                    .filter(path -> path.toFile().isFile() && path.toFile().length() > 0)
+                    .map(FileEvent::new)
+                    .subscribe(applicationEventPublisher::publishEvent);
         } catch (IOException e) {
             log.warn("A problem with scanning:", e);
         }
